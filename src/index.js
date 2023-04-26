5f849553877dfacbf35b348326bd8df75d8c6f31
unknown <mirandamartins05@gmail.com>
2023-04-25 22:24:18 -0300

    adicionei js como, não enviar dados em branco para o terminal

--- src/index.js
+++ src/index.js
@@ -4,17 +4,37 @@ console.log(validator);
 
 //o comando abaixo serve para deixar um espaço para separar os numeros do cartao
 const input = document.querySelector('input')
+    input.addEventListener('keypress', () => {
+        let inputLength = input.value.length
+             if (inputLength === 4 || inputLength === 9 || inputLength == 14) {
+                        input.value += ' '
+                     }
+})
 
-input.addEventListener('keypress', () => {
-     let inputLength = input.value.length
+let numeroDoCartao = document.getElementById ("numeroDoCartao");
+let form           = document.getElementById ("form");
+let textform       = document.getElementById ("textform");
 
-     if (inputLength === 4 || inputLength === 9 || inputLength == 14) {
-             input.value += ' '
-  }
+form.addEventListener("submit" , (event) =>{
+    //nao deixa que envie valor em branco para o terminal
+    if(numeroDoCartao.value == ''){
+        //o textform exibe mensagem 'campo obrigatorio'
+        textform.textContent = 'Campo Obrigatório'
+    }
+    else {
+        //console para ver se os dados estao entranto no terminal
+         console.log(numeroDoCartao.value);
+    }
+
+    //o preventdefault esta empedindo de que a pagina carregue e os dados do cartao no campo sumam
+    event.preventDefault()
 })
 
 
+//o comando abaixo serve para chamar o numeroDoCartao
+/*numeroDoCartao.addEventListener("onkeyup" , () =>){
+    if(validarcartao(numeroDoCartao.value) !== true)
+}*/
 
 
-
-//manipulação do dom vai aqui*/
+//manipulação do dom vai aqui
--- src/validator.js
+++ src/validator.js
@@ -1,14 +1,16 @@
 const validator = {
-  // ...
+  maskify: function (nunCard) {
+
+  },
+
+
+  isValid: function validarcartao(nunCard) {
+    let validarcartao = (/\D/g,"");
+    return validarcartao.test(nunCard)
+  }
+
 };
 
 export default validator;
-
-
-
-
-
-
-
 
 //colocar aqui a logica do projeto
