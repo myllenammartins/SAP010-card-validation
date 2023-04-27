adcaf770ce3f67f879b089ff123462757194fb6f
unknown <mirandamartins05@gmail.com>
2023-04-26 22:49:54 -0300

    comando adicionado no JS para mostrar, assim que estiver digitando o número do cartão, logo abaixo na caixa com # e o replace

--- src/index.js
+++ src/index.js
@@ -2,35 +2,44 @@ import validator from './validator.js';
 
 console.log(validator);
 
-//o comando abaixo serve para deixar um espaço para separar os numeros do cartao
-const input = document.querySelector('input')
-    input.addEventListener('keypress', () => {
-        let inputLength = input.value.length
-             if (inputLength === 4 || inputLength === 9 || inputLength == 14) {
-                        input.value += ' '
-                     }
-})
 
-let numeroDoCartao = document.getElementById ("numeroDoCartao");
+let inputNumero = document.getElementById ("inputNumero");
 let form           = document.getElementById ("form");
 let textform       = document.getElementById ("textform");
+let aparecer       = document.getElementById ("aparecer")
 
-form.addEventListener("submit" , (event) =>{
-    //nao deixa que envie valor em branco para o terminal
-    if(numeroDoCartao.value == ''){
+    form.addEventListener("submit" , (event) =>{
+        //nao deixa que envie valor em branco para o terminal
+        if(inputNumero.value == ''){
         //o textform exibe mensagem 'campo obrigatorio'
-        textform.textContent = 'Campo Obrigatório'
-    }
-    else {
+            textform.textContent = 'Campo Obrigatório'
+        }
+        else {
         //console para ver se os dados estao entranto no terminal
-         console.log(numeroDoCartao.value);
-    }
+         console.log(inputNumero.value);
+         }
     //o preventdefault esta empedindo de que a pagina carregue e os dados do cartao no campo sumam
     event.preventDefault()
-})
+    })
 
+    //para acessar o campo de formulario (ver se os numeros estao entrando cada)
+    inputNumero.addEventListener("keyup" , (e) => {
+        let valorInput = e.target.value;
 
-
+            //o replace é uma expressao regular
+            //tira espaços em brando
+            inputNumero.value = valorInput.replace(/\s/g, '')
+	                                         //tira letras
+	                                      .replace(/\D/g, '')
+	                                         //da espaço a cada quatro numeros
+	                                      .replace(/([0-9]{4})/g, '$1 ')
+	                                        //tira o ultimo espaciado
+	                                      .trim();
+            aparecer.textContent = valorInput;
+                if(valorInput == ''){
+                    aparecer.textContent = '#### #### #### ####';
+                }
+    });
 
 
 
