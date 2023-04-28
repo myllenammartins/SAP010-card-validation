13089d55694eebd4abd1efd5e4b77b4d4f2262a1
unknown <mirandamartins05@gmail.com>
2023-04-27 22:44:12 -0300

    adicionado no validator.js a lógica do isValid

--- src/validator.js
+++ src/validator.js
@@ -3,10 +3,25 @@ const validator = {
     
   },
 
-
-  isValid: function () {
-
+  isValid: function isValid(numero) {
+    const numString = String(numero).replace(/\s/g, '');
+    const numAr = numString.split('').map(Number);
+    const invertAr = numAr.reverse();
+    let soma = 0;
+    for (let i = 0; i < invertAr.length; i++) {
+      if (i % 2 === 1) {
+        let double = invertAr[i] * 2;
+        if (double >= 10) {
+          double = double.toString().split('').map(Number).reduce((a, b) => a + b);
+        }
+        soma += double; // adiciona o valor à soma se o índice for par
+      } else {
+        soma += invertAr[i];
+      }
+    }
+    return soma % 10 === 0;
   }
+  
 
 }
 
